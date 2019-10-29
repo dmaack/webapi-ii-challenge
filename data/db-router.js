61f5640e204fb210b4feb593474e17ce641d4c1b
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-29 15:47:19 -0600

    PUT for post by id

--- data/db-router.js
+++ data/db-router.js
@@ -98,8 +98,27 @@ router.delete('/:id', (req,res) => {
         res.status(500).json({ error: "The post could not be removed" })
     })
 })
-// router.put('/:id', (req, res) => {
 
-// })
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body
+    postId = req.params.id
+
+    if( !title || !contents ) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Data.update(postId, req.body)
+        .then(post => {
+            if(post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+    }
+
+})
 
 module.exports = router;
