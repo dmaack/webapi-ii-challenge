98e11a447489e71c4f0f5882e6d32689f743fba7
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-29 15:20:51 -0600

    GET by id

--- data/db-router.js
+++ data/db-router.js
@@ -51,9 +51,22 @@ router.get('/', (req, res) => { // 4
     })
 })
 
-// router.get('/:id', (req,res) => {
+router.get('/:id', (req,res) => {
+    const postId = req.params.id;
 
-// })
+    Data.findById(postId)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+
+})
 // router.get('/:id/comments', (req,res) => {
 
 // })
