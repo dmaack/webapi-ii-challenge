a062955fbbf56d85f3e652e3876f484f520e7109
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-29 14:44:04 -0600

    POST endpoint using info sent inside req.body

--- data/db-router.js
+++ data/db-router.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 
 // 5
-const Data = require('./db-router.js')
+const Data = require('./db.js')
 
 // 3 
 router.post('/', (req,res) => {
+    const { title, contents } = req.body
 
+    if( !title || !contents ) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Data.insert(req.body)
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    }
 })
 
 // router.post('/:id/comments', (req, res) => {
